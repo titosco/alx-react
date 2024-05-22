31aa3959ed0a770f3639abcf9920d7438ed8004a
titosco <titobiloluwatella@gmail.com>
2024-05-22 22:59:40 +0100

    correcting again

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,13 @@
+
+const path = require("path");
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+};
